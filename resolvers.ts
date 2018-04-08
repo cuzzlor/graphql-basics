efb8fa91116d4a5005f142c0706524b26538de77
Sam Curry <sam.curry@outlook.com>
2018-04-08 21:53:27 +0800

    lesson 4 step 2 code

--- resolvers.ts
+++ resolvers.ts
@@ -10,6 +10,12 @@ export const resolvers: any = {
         artist: async (source: any, { id }: { id: number }) => chinookService.artist(id),
         artistsByName: async (source: any, { nameLike }: { nameLike: string }) =>
             chinookService.artistsByName(nameLike),
+        albums: async () => chinookService.albums(),
+        album: async (source: any, { id }: { id: number }) => chinookService.album(id),
+        albumsByTitle: async (source: any, { titleLike }: { titleLike: string }) =>
+            chinookService.albumsByTitle(titleLike),
+        tracksByComposer: async (source: any, { composerLike }: { composerLike: string }) =>
+            chinookService.tracksByComposer(composerLike),
     },
     Artist: {
         albums: async (source: Artist) => chinookService.albumsByArtist(source.id),
