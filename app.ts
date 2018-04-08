e04b6e9eaa2c350c85be2bba55a0698296fa6b2c
Sam Curry <sam.curry@readify.net>
2018-04-08 12:54:35 +0800

    lesson 1 code

---
+++ app.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(3000, () => console.log('Hello World running at http://localhost:3000'));
